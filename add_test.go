b559fbc33fc0e42af9276c549b7754a6f7683550
catarina.bombaca <catarinaipbombaca@gmail.com>
2022-01-11 15:09:53 +0000

    RED - part 2

--- add_test.go
+++ add_test.go
@@ -6,11 +6,29 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-	expectedValue := 9
-	firstInt := 5
-	secondInt := 4
+	t.Run("given 2 integers 4 and 5, when we add them, then return 9 as a result", func(t *testing.T) {
+		expectedValue := 9
+		firstInt := 5
+		secondInt := 4
 
-	actualValue := Add(firstInt, secondInt)
+		actualValue := Add(firstInt, secondInt)
 
-	assert.Equal(t, expectedValue, actualValue)
+		assert.Equal(t, expectedValue, actualValue)
+	})
+
+	t.Run("given a variable number of integers, when we add them, then return the total sum as a result", func(t *testing.T) {
+		var (
+			number1 = 5
+			number2 = 4
+			number3 = 3
+			number4 = 2
+			number5 = -10
+			expectedResult = 4
+		)
+
+		actualResult := Add(number1, number2, number3, number4, number5)
+
+		assert.Equal(t, expectedResult, actualResult)
+
+	})
 }
