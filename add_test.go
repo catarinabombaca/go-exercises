ac4b81d90a8253e6647dc41edf5df10ddee3cb1e
catarina.bombaca <catarinaipbombaca@gmail.com>
2022-01-11 14:59:45 +0000

    [track-1-part-1]

--- add_test.go
+++ add_test.go
@@ -6,14 +6,11 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-	//given two integers 5 and 4
 	expectedValue := 9
 	firstInt := 5
 	secondInt := 4
 
-	//when we call Add and pass them as arguments
 	actualValue := Add(firstInt, secondInt)
 
-	//should return 9
 	assert.Equal(t, expectedValue, actualValue)
 }
